2fca2ae0bc769d91bddd2bfce185470876752b37
Matteo Ferro <ferro.teo98@gmail.com>
2023-08-29 13:29:09 +0300

    added script for extracting list of patients with both parents (used in new target definition)

---
+++ data/extract_bothparent_patients.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+RELATIVES_PATH = "/data/projects/project_SophieAndZhiyu/Relatives/family_relationships.wMarrigeChild"
+df = pd.read_csv(RELATIVES_PATH)
+occurrences = df[df.relationship == 'parent'].groupby('ID1').size()
+print(f'starting with {len(occurrences)} patients')
+out = occurrences[occurrences==2]
+print(f'{len(out)} patients with both parents')
+registry_ids = out.index.tolist()
+print('saving list of patient into file: both_parents_list.txt ')
+with open(r'/data/projects/project_GNN/gnn_family_pedigree/data/both_parents_list.txt', 'w') as fp:
+    fp.write('\n'.join(registry_ids))
+    
