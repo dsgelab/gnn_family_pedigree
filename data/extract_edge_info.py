1ed5779651383ff144095a2940c603f07ca1310e
Matteo Ferro <ferro.teo98@gmail.com>
2023-08-30 17:11:36 +0300

    changed the target patient outoput

--- data/extract_edge_info.py
+++ data/extract_edge_info.py
@@ -10,17 +10,17 @@ STATFILE_PATH  = PROJECT_PATH + "data/statfile.csv"
 # fetch data
 df = pd.read_csv(RELATIVES_PATH)
 statfile = pd.read_csv(STATFILE_PATH)
+print(f'starting with {df.shape[0]} total edges')
 
 # remove non graph patients
-print(f'starting with {df.shape[0]} rows')
-to_keep = statfile[statfile.graph==1]['FINREGISTRYID'].tolist()
-df = df[ (df.ID1.isin(to_keep)) ].reset_index(drop=True)
-df = df[ (df.ID2.isin(to_keep)) ].reset_index(drop=True)
-print(f'remove non graph patients: {df.shape[0]} rows')
+#to_keep = statfile[statfile.graph==1]['FINREGISTRYID'].tolist()
+#df = df[ (df.ID1.isin(to_keep)) ].reset_index(drop=True)
+#df = df[ (df.ID2.isin(to_keep)) ].reset_index(drop=True)
+# all passed the QC step with new minimal_pheno file
 
 # drop ambiguous relatives
 df = df[df.completeness != 'unknown'].reset_index(drop=True)
-print(f'remove unknown edges: {df.shape[0]} rows')
+print(f'remove unknown edges: {df.shape[0]} edges available now')
 
 # add graph edge weights
 WEIGHT_MAP = {
@@ -38,23 +38,22 @@ df['relationship_type'] = df.relationship + '_' + df.completeness
 df['weight'] = df.relationship_type.map(WEIGHT_MAP)
 
 # remove missing IDs
-df = df.loc[df['ID1'].notna()].reset_index(drop=True)
-df = df.loc[df['ID2'].notna()].reset_index(drop=True)
+df = df[ df.ID1.notna() ].reset_index(drop=True)
+df = df[ df.ID2.notna() ].reset_index(drop=True)
 
 # map to node ids
 NODE_MAP = dict(zip(statfile['FINREGISTRYID'], statfile['node_id']))
 df['node1'] = df['ID1'].map(NODE_MAP).astype('int64')
 df['node2'] = df['ID2'].map(NODE_MAP).astype('int64')
 
+# add target patient info
+target_patients = statfile[statfile.target==1]['FINREGISTRYID'].tolist()
+df['target_patient'] = np.where(df.node1.isin(target_patients), df.node1, -1)
+# find smallest and biggest target subgraphs:
+print(df.groupby('target_patient').value_counts())
+
 # save results
-targets = statfile[statfile.target==1]['FINREGISTRYID'].tolist()
-df['target_patient'] = np.where(df.node1.isin(targets),df.node1,df.node2).astype('int64')
-df = df.sort_values(by=['target_patient'])
-
-print(f'result: \n {df.head(5)}')
-print(f'graph sizes summary: \n {df.target_patient.value_counts()}')
-
-df_edge = df[['node1','node2','target_patient','weight','relationship_type']]
+df_edge = df[['node1','node2','target_patient','relationship','relationship_type','weight']]
 df_edge.to_csv(PROJECT_PATH+"data/edgefile.csv", index=None)
 
 gc.collect()
