a586b91f85ae8ced6826ee3c387ebb9ac5cb3e2d
ferroteo <ferro.teo98@gmail.com>
2024-02-15 14:49:59 +0200

    1st model train hyperparameters

--- run_model.sh
+++ run_model.sh
@@ -1,6 +1,6 @@
 source /data/projects/project_GNN/GAT_family_pedigree/env/bin/activate
 
-statfile=/data/projects/project_GNN/GAT_family_pedigree/data/statfile.csv
+statfile=/data/projects/project_GNN/GAT_family_pedigree/data/statfile_with_relationships.csv
 maskfile=/data/projects/project_GNN/GAT_family_pedigree/data/maskfile.csv
 featfile=/data/projects/project_GNN/GAT_family_pedigree/data/featfile_chd.csv 
 
@@ -11,8 +11,8 @@ directed=True
 add_self_loops=False
 pooling_method=target
 num_workers=20
-patience=20
-max_epochs=50
+patience=10
+max_epochs=10
 learning_rate=0.01
 hidden_dim=480
 hidden_dim_2=240
@@ -29,8 +29,8 @@ batchsize=10
 device='cuda:1' 
 
 model_type=graph
-outpath=output/GAT_test0/
+aggr_func=mean
+outpath=output/GAT_train0/
+experiment=CHD_GAT_mean
 
-aggr_func=max
-experiment=chd_GAT_max
-nohup python3 ./src/main.py --aggr_func ${aggr_func} --featfile ${featfile} --model_type ${model_type} --experiment ${experiment} --batchsize ${batchsize} --outpath ${outpath} --statfile ${statfile} --maskfile ${maskfile} --mask_target ${mask_target} --gnn_layer ${gnn_layer} --use_edge ${use_edge} --pooling_method ${pooling_method} --num_workers ${num_workers} --max_epochs ${max_epochs} --patience ${patience} --learning_rate ${learning_rate} --hidden_dim ${hidden_dim} --loss ${loss} --gamma ${gamma} --alpha ${alpha} --beta ${beta} --delta ${delta} --dropout_rate ${dropout_rate} --threshold_opt ${threshold_opt} --device ${device} > ${outpath}/${experiment}.txt 
+python3 ./src/main.py --aggr_func ${aggr_func} --featfile ${featfile} --model_type ${model_type} --experiment ${experiment} --batchsize ${batchsize} --outpath ${outpath} --statfile ${statfile} --maskfile ${maskfile} --mask_target ${mask_target} --gnn_layer ${gnn_layer} --use_edge ${use_edge} --pooling_method ${pooling_method} --num_workers ${num_workers} --max_epochs ${max_epochs} --patience ${patience} --learning_rate ${learning_rate} --hidden_dim ${hidden_dim} --loss ${loss} --gamma ${gamma} --alpha ${alpha} --beta ${beta} --delta ${delta} --dropout_rate ${dropout_rate} --threshold_opt ${threshold_opt} --device ${device} > ${outpath}/${experiment}.txt 
